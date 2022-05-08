b90c091a7147dafd645d95257a8c5b5193c87344
LotronStudios <dowranrowshenow@gmail.com>
2022-05-08 17:22:04 +0500

    Commit

--- app.js
+++ app.js
@@ -8,10 +8,12 @@ function sideNav()
     {
         side_nav.style.width = "0px";
         active.style.background = "rgba(0, 0, 0, 0)";
+        side_nav.style.opacity = 0;
     }
     else
     {
         side_nav.style.width = "250px";
         active.style.background = "rgba(0, 0, 0, 0.1)";
+        side_nav.style.opacity = 1;
     }
 }
